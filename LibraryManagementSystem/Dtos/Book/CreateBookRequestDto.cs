862e39eee338868448b96fc3755075ffa309f2a0
TomasWild <tomaswild@outlook.com>
2025-02-07 17:24:25 -0300

    feat: add validation

--- LibraryManagementSystem/Dtos/Book/CreateBookRequestDto.cs
+++ LibraryManagementSystem/Dtos/Book/CreateBookRequestDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LibraryManagementSystem.Dtos.Book;
 
 public class CreateBookRequestDto
 {
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(100, ErrorMessage = "Title must not exceed 100 characters.")]
     public required string Title { get; set; }
+
+    [StringLength(1000, ErrorMessage = "Synopsis must not exceed 1000 characters.")]
     public string? Synopsis { get; set; }
+
     public int AuthorId { get; set; }
+
+    [MinLength(1, ErrorMessage = "At least one category must be added.")]
     public List<int> CategoryIds { get; set; } = [];
 }
