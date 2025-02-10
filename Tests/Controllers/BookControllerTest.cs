6990b20ba733c43e96242eb8c163f480c2f907b1
TomasWild <tomaswild@outlook.com>
2025-02-10 19:31:00 -0300

    refactor(tests): improve setup

--- Tests/Controllers/BookControllerTest.cs
+++ Tests/Controllers/BookControllerTest.cs
@@ -12,34 +12,41 @@ namespace Tests.Controllers;
 
 public class BookControllerTest
 {
+    private readonly Mock<IBookRepository> _mockRepository;
+    private readonly Mock<IMapper> _mockMapper;
+    private readonly BookController _controller;
+
+    public BookControllerTest()
+    {
+        _mockRepository = new Mock<IBookRepository>();
+        _mockMapper = new Mock<IMapper>();
+        var mockService = new Mock<ICacheService>();
+        _controller = new BookController(_mockRepository.Object, _mockMapper.Object, mockService.Object);
+    }
+
     [Fact]
     public async Task CreateBook_ValidRequest_ReturnsCreatedAtAction()
     {
         // Arrange
-        var mockRepository = new Mock<IBookRepository>();
-        var mockMapper = new Mock<IMapper>();
-        var mockService = new Mock<ICacheService>();
-        var controller = new BookController(mockRepository.Object, mockMapper.Object, mockService.Object);
-
         var bookRequest = new CreateBookRequestDto
             { Title = "Book Title", Synopsis = "Book synopsis.", CategoryIds = [1] };
         var book = new Book { Id = 1, Title = "Book Title", Synopsis = "Book synopsis." };
         var bookDto = new BookDto
             { Id = 1, Title = "Book Title", Synopsis = "Book synopsis.", AuthorName = "Author Name" };
 
-        mockRepository.Setup(r => r.CreateBookAsync(It.IsAny<Book>(), It.IsAny<IEnumerable<int>>()))
+        _mockRepository.Setup(r => r.CreateBookAsync(It.IsAny<Book>(), It.IsAny<IEnumerable<int>>()))
             .ReturnsAsync(book);
-        mockMapper.Setup(m => m.Map<Book>(It.IsAny<CreateBookRequestDto>()))
+        _mockMapper.Setup(m => m.Map<Book>(It.IsAny<CreateBookRequestDto>()))
             .Returns(book);
-        mockMapper.Setup(m => m.Map<BookDto>(It.IsAny<Book>()))
+        _mockMapper.Setup(m => m.Map<BookDto>(It.IsAny<Book>()))
             .Returns(bookDto);
 
         // Act
-        var result = await controller.CreateBook(bookRequest);
+        var result = await _controller.CreateBook(bookRequest);
 
         // Assert
         var actionResult = Assert.IsType<CreatedAtActionResult>(result);
-        mockRepository.Verify(r => r.CreateBookAsync(book, bookRequest.CategoryIds), Times.Once);
+        _mockRepository.Verify(r => r.CreateBookAsync(book, bookRequest.CategoryIds), Times.Once);
         Assert.Equal(nameof(BookController.GetBookById), actionResult.ActionName);
         Assert.Equal(bookDto, actionResult.Value);
     }
@@ -48,12 +55,7 @@ public class BookControllerTest
     public async Task CreateBook_InvalidRequest_ReturnsBadRequest()
     {
         // Arrange
-        var mockRepository = new Mock<IBookRepository>();
-        var mockMapper = new Mock<IMapper>();
-        var mockService = new Mock<ICacheService>();
-        var controller = new BookController(mockRepository.Object, mockMapper.Object, mockService.Object);
-
-        controller.ModelState.AddModelError("Title", "The Title field is required.");
+        _controller.ModelState.AddModelError("Title", "The Title field is required.");
 
         var invalidBookRequest = new CreateBookRequestDto
         {
@@ -63,7 +65,7 @@ public class BookControllerTest
         };
 
         // Act
-        var result = await controller.CreateBook(invalidBookRequest);
+        var result = await _controller.CreateBook(invalidBookRequest);
 
         // Assert
         var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
@@ -78,11 +80,6 @@ public class BookControllerTest
     public async Task GetAllBooks_BooksExist_ReturnsOkWithMappedDtos()
     {
         // Arrange
-        var mockRepository = new Mock<IBookRepository>();
-        var mockMapper = new Mock<IMapper>();
-        var mockService = new Mock<ICacheService>();
-        var controller = new BookController(mockRepository.Object, mockMapper.Object, mockService.Object);
-
         var books = new List<Book>
         {
             new() { Id = 1, Title = "Book Title 1", Synopsis = "Book 1 synopsis." },
@@ -90,9 +87,9 @@ public class BookControllerTest
             new() { Id = 3, Title = "Book Title 3", Synopsis = "Book 3 synopsis." }
         };
 
-        mockRepository.Setup(r => r.GetAllBooksAsync(It.IsAny<QueryObject>()))
+        _mockRepository.Setup(r => r.GetAllBooksAsync(It.IsAny<QueryObject>()))
             .ReturnsAsync(books);
-        mockMapper.Setup(m => m.Map<List<BookDto>>(It.IsAny<List<Book>>()))
+        _mockMapper.Setup(m => m.Map<List<BookDto>>(It.IsAny<List<Book>>()))
             .Returns((List<Book> src) => src.Select(b => new BookDto
             {
                 Id = b.Id,
@@ -102,7 +99,7 @@ public class BookControllerTest
             }).ToList());
 
         // Act
-        var result = await controller.GetAllBooks(It.IsAny<QueryObject>()) as OkObjectResult;
+        var result = await _controller.GetAllBooks(It.IsAny<QueryObject>()) as OkObjectResult;
 
         // Assert
         Assert.NotNull(result);
@@ -117,18 +114,13 @@ public class BookControllerTest
     public async Task GetAllBooks_NoBooksExist_ReturnsEmptyList()
     {
         // Arrange
-        var mockRepository = new Mock<IBookRepository>();
-        var mockMapper = new Mock<IMapper>();
-        var mockService = new Mock<ICacheService>();
-        var controller = new BookController(mockRepository.Object, mockMapper.Object, mockService.Object);
-
-        mockRepository.Setup(r => r.GetAllBooksAsync(It.IsAny<QueryObject>()))
+        _mockRepository.Setup(r => r.GetAllBooksAsync(It.IsAny<QueryObject>()))
             .ReturnsAsync([]);
-        mockMapper.Setup(m => m.Map<List<BookDto>>(It.IsAny<List<Book>>()))
+        _mockMapper.Setup(m => m.Map<List<BookDto>>(It.IsAny<List<Book>>()))
             .Returns([]);
 
         // Act
-        var result = await controller.GetAllBooks(It.IsAny<QueryObject>()) as OkObjectResult;
+        var result = await _controller.GetAllBooks(It.IsAny<QueryObject>()) as OkObjectResult;
 
         // Assert
         Assert.NotNull(result);
@@ -142,24 +134,19 @@ public class BookControllerTest
     public async Task GetBookById_WhenTodoExists_ReturnsTodoItemDto()
     {
         // Arrange
-        var mockRepository = new Mock<IBookRepository>();
-        var mockMapper = new Mock<IMapper>();
-        var mockService = new Mock<ICacheService>();
-        var controller = new BookController(mockRepository.Object, mockMapper.Object, mockService.Object);
-
-        mockRepository.Setup(r => r.GetBookByIdAsync(It.IsAny<int>()))
+        _mockRepository.Setup(r => r.GetBookByIdAsync(It.IsAny<int>()))
             .ReturnsAsync(new Book { Id = 1, Title = "Book Title 1", Synopsis = "Book 1 synopsis." });
-        mockMapper.Setup(m => m.Map<BookDto>(It.IsAny<Book>()))
+        _mockMapper.Setup(m => m.Map<BookDto>(It.IsAny<Book>()))
             .Returns(new BookDto
                 { Id = 1, Title = "Book Title", Synopsis = "Book synopsis.", AuthorName = "Author Name" });
 
         // Act
-        var result = await controller.GetBookById(1);
+        var result = await _controller.GetBookById(1);
 
         // Assert
         var actionResult = Assert.IsType<OkObjectResult>(result);
         var returnedBook = Assert.IsType<BookDto>(actionResult.Value);
-        mockRepository.Verify(r => r.GetBookByIdAsync(It.IsAny<int>()), Times.Once);
+        _mockRepository.Verify(r => r.GetBookByIdAsync(It.IsAny<int>()), Times.Once);
         Assert.NotNull(returnedBook);
         Assert.Equal(1, returnedBook.Id);
         Assert.Equal(200, actionResult.StatusCode);
@@ -169,19 +156,14 @@ public class BookControllerTest
     public async Task GetBookById_BookNotFound_ReturnsNotFound()
     {
         // Arrange
-        var mockRepository = new Mock<IBookRepository>();
-        var mockMapper = new Mock<IMapper>();
-        var mockService = new Mock<ICacheService>();
-        var controller = new BookController(mockRepository.Object, mockMapper.Object, mockService.Object);
-
-        mockRepository.Setup(r => r.GetBookByIdAsync(It.IsAny<int>()))
+        _mockRepository.Setup(r => r.GetBookByIdAsync(It.IsAny<int>()))
             .ReturnsAsync((Book?)null);
 
         // Act
-        var result = await controller.GetBookById(1);
+        var result = await _controller.GetBookById(1);
 
         // Assert
-        mockRepository.Verify(r => r.GetBookByIdAsync(It.IsAny<int>()), Times.Once);
+        _mockRepository.Verify(r => r.GetBookByIdAsync(It.IsAny<int>()), Times.Once);
         Assert.IsType<NotFoundResult>(result);
     }
 
@@ -189,11 +171,6 @@ public class BookControllerTest
     public async Task UpdateBook_ReturnsUpdatedBook_WhenBookExists()
     {
         // Arrange
-        var mockRepository = new Mock<IBookRepository>();
-        var mockMapper = new Mock<IMapper>();
-        var mockService = new Mock<ICacheService>();
-        var controller = new BookController(mockRepository.Object, mockMapper.Object, mockService.Object);
-
         var existingBook = new Book
         {
             Id = 1, Title = "Book Title", Synopsis = "Book synopsis.",
@@ -203,14 +180,14 @@ public class BookControllerTest
         var bookRequest = new UpdateBookRequestDto
             { Title = "Updated Book Title", Synopsis = "Updated book synopsis." };
 
-        mockRepository.Setup(r => r.UpdateBookByIdAsync(existingBook.Id, bookRequest))
+        _mockRepository.Setup(r => r.UpdateBookByIdAsync(existingBook.Id, bookRequest))
             .ReturnsAsync(new Book
             {
                 Id = existingBook.Id, Title = existingBook.Title, Synopsis = existingBook.Synopsis,
                 AuthorId = existingBook.AuthorId,
                 Author = new Author { FirstName = "Author First Name", LastName = "Author Last Name" }
             });
-        mockMapper.Setup(m => m.Map<BookDto>(It.IsAny<Book>()))
+        _mockMapper.Setup(m => m.Map<BookDto>(It.IsAny<Book>()))
             .Returns((Book src) => new BookDto
             {
                 Id = src.Id, Title = src.Title, Synopsis = src.Synopsis,
@@ -218,7 +195,7 @@ public class BookControllerTest
             });
 
         // Act
-        var result = await controller.UpdateBook(existingBook.Id, bookRequest);
+        var result = await _controller.UpdateBook(existingBook.Id, bookRequest);
 
         // Assert
         var actionResult = Assert.IsType<OkObjectResult>(result);
@@ -231,22 +208,17 @@ public class BookControllerTest
     public async Task UpdateBook_NonExistentBook_ReturnsNotFound()
     {
         // Arrange
-        var mockRepository = new Mock<IBookRepository>();
-        var mockMapper = new Mock<IMapper>();
-        var mockService = new Mock<ICacheService>();
-        var controller = new BookController(mockRepository.Object, mockMapper.Object, mockService.Object);
-
         var updateRequest = new UpdateBookRequestDto
         {
             Title = "Updated Title",
             Synopsis = "Updated Synopsis"
         };
 
-        mockRepository.Setup(r => r.UpdateBookByIdAsync(It.IsAny<int>(), It.IsAny<UpdateBookRequestDto>()))
+        _mockRepository.Setup(r => r.UpdateBookByIdAsync(It.IsAny<int>(), It.IsAny<UpdateBookRequestDto>()))
             .ReturnsAsync((Book?)null);
 
         // Act
-        var result = await controller.UpdateBook(999, updateRequest);
+        var result = await _controller.UpdateBook(999, updateRequest);
 
         // Assert
         Assert.IsType<NotFoundResult>(result);
@@ -256,21 +228,16 @@ public class BookControllerTest
     public async Task DeleteBook_ValidId_ReturnsNoContent()
     {
         // Arrange
-        var mockRepository = new Mock<IBookRepository>();
-        var mockMapper = new Mock<IMapper>();
-        var mockService = new Mock<ICacheService>();
-        var controller = new BookController(mockRepository.Object, mockMapper.Object, mockService.Object);
-
         var book = new Book { Id = 1, Title = "Book Title", Synopsis = "Book synopsis." };
 
-        mockRepository.Setup(r => r.DeleteBookByIdAsync(book.Id))
+        _mockRepository.Setup(r => r.DeleteBookByIdAsync(book.Id))
             .ReturnsAsync(book);
 
         // Act
-        var result = await controller.DeleteBook(1);
+        var result = await _controller.DeleteBook(1);
 
         // Assert
-        mockRepository.Verify(r => r.DeleteBookByIdAsync(book.Id), Times.Once);
+        _mockRepository.Verify(r => r.DeleteBookByIdAsync(book.Id), Times.Once);
         Assert.IsType<NoContentResult>(result);
     }
 
@@ -278,19 +245,14 @@ public class BookControllerTest
     public async Task DeleteBook_InvalidId_ReturnsNotFound()
     {
         // Arrange
-        var mockRepository = new Mock<IBookRepository>();
-        var mockMapper = new Mock<IMapper>();
-        var mockService = new Mock<ICacheService>();
-        var controller = new BookController(mockRepository.Object, mockMapper.Object, mockService.Object);
-
-        mockRepository.Setup(r => r.DeleteBookByIdAsync(It.IsAny<int>()))
+        _mockRepository.Setup(r => r.DeleteBookByIdAsync(It.IsAny<int>()))
             .ReturnsAsync((Book?)null);
 
         // Act
-        var result = await controller.DeleteBook(1);
+        var result = await _controller.DeleteBook(1);
 
         // Assert
-        mockRepository.Verify(r => r.DeleteBookByIdAsync(1), Times.Once);
+        _mockRepository.Verify(r => r.DeleteBookByIdAsync(1), Times.Once);
         Assert.IsType<NotFoundResult>(result);
     }
 }
--- Tests/Services/CacheServiceTest.cs
+++ Tests/Services/CacheServiceTest.cs
@@ -8,23 +8,29 @@ namespace Tests.Services;
 
 public class CacheServiceTest
 {
+    private readonly Mock<IDistributedCache> _mockCache;
+    private readonly CacheService _cacheService;
+
+    public CacheServiceTest()
+    {
+        _mockCache = new Mock<IDistributedCache>();
+        _cacheService = new CacheService(_mockCache.Object);
+    }
+
     [Fact]
     public async Task GetAsync_ReturnsCachedValue_WhenKeyExists()
     {
         // Arrange
-        var mockCache = new Mock<IDistributedCache>();
-        var cacheService = new CacheService(mockCache.Object);
-
         const string key = "test_key";
         var value = new TestObject { Id = 1, Name = "Test" };
         var serializeValue = JsonSerializer.Serialize(value);
         var encodedValue = Encoding.UTF8.GetBytes(serializeValue);
 
-        mockCache.Setup(c => c.GetAsync(key, It.IsAny<CancellationToken>()))
+        _mockCache.Setup(c => c.GetAsync(key, It.IsAny<CancellationToken>()))
             .ReturnsAsync(encodedValue);
 
         // Act
-        var result = await cacheService.GetAsync<TestObject>(key);
+        var result = await _cacheService.GetAsync<TestObject>(key);
 
         // Assert
         Assert.NotNull(result);
@@ -36,16 +42,13 @@ public class CacheServiceTest
     public async Task GetAsync_ReturnsNull_WhenKeyDoesNotExist()
     {
         // Arrange
-        var mockCache = new Mock<IDistributedCache>();
-        var cacheService = new CacheService(mockCache.Object);
-
         const string key = "non_existent_key";
 
-        mockCache.Setup(c => c.GetAsync(key, It.IsAny<CancellationToken>()))
+        _mockCache.Setup(c => c.GetAsync(key, It.IsAny<CancellationToken>()))
             .ReturnsAsync((byte[]?)null);
 
         // Act
-        var result = await cacheService.GetAsync<TestObject>(key);
+        var result = await _cacheService.GetAsync<TestObject>(key);
 
         // Assert
         Assert.Null(result);
@@ -55,15 +58,12 @@ public class CacheServiceTest
     public async Task SetAsync_SetsValueInCache()
     {
         // Arrange
-        var mockCache = new Mock<IDistributedCache>();
-        var cacheService = new CacheService(mockCache.Object);
-
         const string key = "test_key";
         var value = new TestObject { Id = 1, Name = "Test" };
         var serializedValue = JsonSerializer.Serialize(value);
         var encodedValue = Encoding.UTF8.GetBytes(serializedValue);
 
-        mockCache
+        _mockCache
             .Setup(c => c.SetAsync(
                 key,
                 It.Is<byte[]>(b => b.SequenceEqual(encodedValue)),
@@ -72,10 +72,10 @@ public class CacheServiceTest
             .Returns(Task.CompletedTask);
 
         // Act
-        await cacheService.SetAsync(key, value);
+        await _cacheService.SetAsync(key, value);
 
         // Assert
-        mockCache.Verify(
+        _mockCache.Verify(
             c => c.SetAsync(
                 key,
                 It.Is<byte[]>(b => b.SequenceEqual(encodedValue)),
@@ -88,19 +88,16 @@ public class CacheServiceTest
     public async Task RemoveAsync_RemovesValueFromCache()
     {
         // Arrange
-        var mockCache = new Mock<IDistributedCache>();
-        var cacheService = new CacheService(mockCache.Object);
-
         const string key = "test_key";
 
-        mockCache.Setup(c => c.RemoveAsync(key, It.IsAny<CancellationToken>()))
+        _mockCache.Setup(c => c.RemoveAsync(key, It.IsAny<CancellationToken>()))
             .Returns(Task.CompletedTask);
 
         // Act
-        await cacheService.RemoveAsync(key);
+        await _cacheService.RemoveAsync(key);
 
         // Assert
-        mockCache.Verify(c => c.RemoveAsync(key, It.IsAny<CancellationToken>()), Times.Once);
+        _mockCache.Verify(c => c.RemoveAsync(key, It.IsAny<CancellationToken>()), Times.Once);
     }
 
     private class TestObject
